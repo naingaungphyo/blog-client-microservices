2258a9d3276226a830f5d5381fcee2ad8315a7dc
naingaungphyo <5661543+naingaungphyo@users.noreply.github.com>
2020-08-13 00:58:00 +0900

    changed to send GET request from query service

--- src/PostList.js
+++ src/PostList.js
@@ -7,7 +7,7 @@ export default () => {
   const [posts, setPosts] = useState({})
 
   const fetchPosts = async () => {
-    const res = await axios.get('http://localhost:4000/posts')
+    const res = await axios.get('http://localhost:4002/posts')
     setPosts(res.data)
   }
 
@@ -20,7 +20,7 @@ export default () => {
       <div key={post.id} className="card" style={{ width: '30%', marginBottom: '20px' }}>
         <div className="card-body">
           <h3>{post.title}</h3>
-          <CommentList postId={post.id} />
+          <CommentList comments={post.comments} />
           <CommentCreate postId={post.id} />
         </div>
       </div>
